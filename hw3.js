625cc43e63931c55ca300b125d680c91392dba5d
Roman Kutselepa <roman.kutselepa@gmail.com>
2023-08-02 14:46:01 +0300

    Update hw3.js

--- hw3.js
+++ hw3.js
@@ -3,14 +3,6 @@
 
 const numsArray = [1, 12, 34, 71, 14, 12, 33, 70, 82, 81, 9, 19, 90];
 
-let sum = 0;
-numsArray.forEach(num => {
-    if (num % 2 === 0) {
-        sum += num
-    }
-})
-
-console.log(sum);
 
 
 // Напишите программу которая проанализирует данный массив с объектами
@@ -42,28 +34,13 @@ const people = [
         age: 17
     }
 ]
-const adults = [];
-people.forEach(person => {
-    if (person.age >= 18) {
-        adults.push(person.name)
-    }
-})
 
-console.log(adults);
 
 
 // напишите программу которая проанализирует данный массив и выведет в консоль самую длинную строку
 
 const strings = ['Star', 'Planet', 'Comet', 'Interstellar', 'Space'];
-let longest = '';
 
-strings.forEach(str => {
-    if (str.length > longest.length) {
-        longest = str;
-    }
-})
-
-console.log(longest);
 
 
 // напишите программу которая возьмёт из данного массива наисенования книг которые вышли в этом году
@@ -96,29 +73,11 @@ const books = [
         release: 2023
     }
 ];
-const newBooks = [];
-
-books.forEach(book => {
-    if (book.release === 2023) {
-        newBooks.push(book.title);
-    }
-})
-
-console.log(newBooks);
 
 
 // Напишите программу которая проанализирует данный массив и найдёт в нем наибольшее число кратное трём
 
 const numbers = [3, 15, 23, 56, 66, 71, 12, 90];
-let highest = 0;
-
-numbers.forEach(num => {
-    if (num > highest && num % 3 === 0) {
-        highest = num;
-    }
-})
-
-console.log(highest);
 
 
 // напишите программу которая найдёт самого старшего человека в данном массиве с объектами
@@ -149,15 +108,3 @@ const people2 = [
         age: 17
     }
 ];
-let oldestPerson;
-
-people2.forEach(person => {
-    if (!oldestPerson) {
-        oldestPerson = person;
-    }
-    if (person.age > oldestPerson.age) {
-        oldestPerson = person;
-    }
-})
-
-console.log(oldestPerson);
